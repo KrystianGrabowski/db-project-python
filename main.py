ccd0547b7e5b5793091a4123355669f036099cb7
KrystianGrabowski <krystian987543@gmail.com>
2019-06-01 22:22:41 +0200

    Program arguments argparse

--- main.py
+++ main.py
@@ -1,28 +1,14 @@
 import psycopg2
 import json
 from Crypto.Cipher import AES
-import sys
-import getopt
 import argparse
 
-
-print("Hello")
-
 def main():
-    try:
-        opts, args = getopt.getopt(sys.argv[1:], "h", ["help", "init"])
-    except getopt.GetoptError:
-        usage()                         
-        sys.exit(2)
-    for o, a in opts:
-        if opt in ("-h", "--help"):
-            #usage()                   
-            sys.exit(0)                               
-        elif opt in ("--init"):
-            grammar = arg
-
-def usage():
-    parser = argparse.ArgumentParser(description='')
+    parser = argparse.ArgumentParser(description='Projekt: System Zarządzania Partią Polityczną')
+    parser.add_argument("-i","--init", help="initialization of database", action="store_true")
+    parser.add_argument("-f", "--filename", nargs=1, help="name of the file with JSON objects", type=str)
+    args = parser.parse_args()
+    print(args.filename)
 
 
 if __name__ == "__main__":
