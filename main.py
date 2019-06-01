43837d4f090ad54bd13bc32a9c7d83b7ed8e5ca2
KrystianGrabowski <krystian987543@gmail.com>
2019-06-02 00:24:45 +0200

    Connection to database

--- main.py
+++ main.py
@@ -5,28 +5,56 @@ import argparse
 
 class Database:
     def open_database(self):
-        conn = psycopg2.connect("dbname=test")
+        self.conn = psycopg2.connect("dbname=test user=krystian password=password")
+        self.cur = self.conn.cursor()
+        self.cur.execute("CREATE TABLE tabeleczka (id integer, val varchar);")
+        self.conn.commit()
     
     def read_from_file(self, file_name):
         self.f = open(file_name, 'r')
         for line in self.f:
-            y = json.loads(line)
-            print(next(iter(y)))
+            f_dict = json.loads(line)
+            key = next(iter(f_dict))
+            self.function_interpreter(key, f_dict[key])
         self.f.close()
 
     def function_interpreter(self, name, args):
         if name == 'open':
             print("open function")
+
         elif name == 'leader':
-            print("open function")
-            
+            print("leader function")
+
+        elif name == 'protest':
+            print("protest function")
+
+        elif name == 'support':
+            print("support function")
+
+        elif name == 'upvote':
+            print("upvote function")
+
+        elif name == 'downvote':
+            print("downvote function")
+
+        elif name == 'actions':
+            print("actions function")
+
+        elif name == 'projects':
+            print("projects function")
+
+        elif name == 'votes':
+            print("votes function")
+
+        elif name == 'trolls':
+            print("trolls function")
+
 def main():
     parser = argparse.ArgumentParser(description='Projekt: System Zarządzania Partią Polityczną')
     parser.add_argument("-i","--init", help="initialization of database", action="store_true")
     parser.add_argument("-f", "--filename", nargs=1, help="name of the file with JSON objects", type=str)
     args = parser.parse_args()
     db = Database()
-    #db.open_database()
     print(args.filename[0])
     db.read_from_file(args.filename[0])
     db.open_database()
