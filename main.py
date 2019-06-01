c88a5de82e8568dadcbed2abf80062267c7069a4
KrystianGrabowski <krystian987543@gmail.com>
2019-06-01 20:37:28 +0200

    Program arguments

--- main.py
+++ main.py
@@ -1,5 +1,31 @@
 import psycopg2
 import json
 from Crypto.Cipher import AES
+import sys
+import getopt
+import argparse
+
 
 print("Hello")
+
+def main():
+    try:
+        opts, args = getopt.getopt(sys.argv[1:], "h", ["help", "init"])
+    except getopt.GetoptError:
+        usage()                         
+        sys.exit(2)
+    for o, a in opts:
+        if opt in ("-h", "--help"):
+            #usage()                   
+            sys.exit(0)                               
+        elif opt in ("--init"):
+            grammar = arg
+
+def usage():
+    parser = argparse.ArgumentParser(description='')
+
+
+if __name__ == "__main__":
+    main()
+
+
