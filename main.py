6fe36cedfe33dccd03e389b6c71ac8380ab22641
KrystianGrabowski <krystian987543@gmail.com>
2019-06-01 23:39:41 +0200

    Working with file

--- main.py
+++ main.py
@@ -1,14 +1,35 @@
 import psycopg2
 import json
-from Crypto.Cipher import AES
+#from Crypto.Cipher import AES
 import argparse
 
+class Database:
+    def open_database(self):
+        conn = psycopg2.connect("dbname=test")
+    
+    def read_from_file(self, file_name):
+        self.f = open(file_name, 'r')
+        for line in self.f:
+            y = json.loads(line)
+            print(next(iter(y)))
+        self.f.close()
+
+    def function_interpreter(self, name, args):
+        if name == 'open':
+            print("open function")
+        elif name == 'leader':
+            print("open function")
+            
 def main():
     parser = argparse.ArgumentParser(description='Projekt: System Zarządzania Partią Polityczną')
     parser.add_argument("-i","--init", help="initialization of database", action="store_true")
     parser.add_argument("-f", "--filename", nargs=1, help="name of the file with JSON objects", type=str)
     args = parser.parse_args()
-    print(args.filename)
+    db = Database()
+    #db.open_database()
+    print(args.filename[0])
+    db.read_from_file(args.filename[0])
+    db.open_database()
 
 
 if __name__ == "__main__":
